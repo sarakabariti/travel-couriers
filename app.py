946d09931c030b40bf127ce1582a29d6655525e1
sarkab <sairkab@hotmail.com>
2020-04-16 11:26:10 +0200

    backend

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, request, redirect, session
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['secret_key'] = "secretkey"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\DELL\\Documents\\foundations\\travel-couriers\\database.db'
+db = SQLAlchemy(app)  
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class contact_us(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    subject = db.Column(db.String(1000), nullable=False)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/how')
+def how():
+    return render_template("how.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
