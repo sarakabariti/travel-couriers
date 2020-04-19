2c452f1bc6ddf73f68a85a8ffb24c0b65de3c86d
sarkab <sairkab@hotmail.com>
2020-04-19 19:08:57 +0200

    backend log-in/sign up

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 
 app = Flask(__name__)
@@ -27,6 +28,48 @@ class contact_us(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == "POST":
+        req = request.form 
+        for x in req:
+            if x == "firstname":
+                _init__ = None
+                try:
+                    firstname = request.form['firstname']
+                    lastname = request.form['lastname']
+                    email = request.form['email']
+                    password = request.form["password"]
+
+                    register = user(firstname=firstname,
+                                    lastname=lastname,
+                                    email=email,
+                                    password=password)
+                    db.session.add(register)
+                    db.session.commit()
+
+                    Message = "success"
+                    return render_template("index.html", Message_up=Message)
+
+                except exc.IntegrityError: 
+                    db.session.rollback()
+
+                    error_up = "We already have user with same email or password"
+                    return render_template("index.html", Message_up=error_up)
+
+            else:
+                email = request.form['email']
+                password = request.form["password"]
+
+                login = user.query.filter_by(
+                    email=email, password=password).first()
+
+                if login is not None:
+                    Message_success = "success"
+                    return render_template("index.html", Message=Message_success)
+
+                if login is None:
+                    error = "Incorrect combination of password or email"
+                    return render_template("index.html", Message=error)
+
     return render_template("index.html")
 
 
@@ -49,6 +92,7 @@ def contact():
                              subject=subject)
         db.session.add(contact)
         db.session.commit()
+        return render_template("contact-form_success.html")
 
     return render_template("contact.html")
 
