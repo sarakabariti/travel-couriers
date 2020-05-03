45ca0afe7f3f919875210304b3776ab82c963710
sarkab <sairkab@hotmail.com>
2020-05-04 00:49:39 +0200

    requests filters, search bar

--- static/js/script.js
+++ static/js/script.js
@@ -55,6 +55,7 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
+  console.log("hello");
 });
 
 
